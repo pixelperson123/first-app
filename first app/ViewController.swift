547bf397a1cf69269439d2cd2a0067b9b01eef07
Suzanne Croston <CROSTON@suzannes-imac-2.home.gateway>
2016-09-25 15:18:49 +0800

    Initial Commit

---
+++ first app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  first app
+//
+//  Created by Suzanne Croston on 25/09/2016.
+//  Copyright © 2016 Jude Croston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
